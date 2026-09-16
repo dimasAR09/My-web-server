7a2d053633ed09f2ff17940121ad99600e93725c
Dimas arif rahmansyah <dimasrahmansyaharif09@Gmail.com>
2026-09-16 21:17:59 +0700

    gambar

--- untitled/webpack.config.prod.js
+++ untitled/webpack.config.prod.js
@@ -12,6 +12,7 @@ module.exports = merge(common, {
     new CopyPlugin({
       patterns: [
         { from: 'css', to: 'css' },
+        { from: 'img', to: 'img' },
         { from: 'icon.svg', to: 'icon.svg' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
